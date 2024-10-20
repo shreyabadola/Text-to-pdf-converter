4047de24a8835eb285318ec780301455a0a63bc7
Shreya Badola <shreyabadola234@gmail.com>
2024-10-20 17:06:55 +0530

    update

--- project_1.py
+++ project_1.py
@@ -4,7 +4,7 @@ from reportlab.pdfgen import canvas
 from reportlab.lib import colors
 
 
-pdf_path = "D:\python\python_Project\Text-to-pdf-converter\text_to_pdf.pdf"
+pdf_path = "D:\python\python_Project\Text-to-pdf-converter\text_to_pdf.pdfpy"
 
 directory = os.path.dirname(pdf_path)
 if not os.path.exists(directory):
